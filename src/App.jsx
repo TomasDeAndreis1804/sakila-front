ac7c9736c286abd481986e981aa353cdb81ff13c
TDAR1804 <tomasdeandreis@gmail.com>
2025-04-03 23:26:38 -0500

    cambios front

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Link, Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/"/>
+    <div className="min-h-screen bg-gray-900 text-white font-sans">
+      <nav className="bg-gray-800 p-4 shadow-lg flex justify-between items-center">
+        <Link to="/" className="text-3xl font-extrabold text-blue-400 hover:text-blue-300">
+          MovieRentals
+        </Link>
       </nav>
-      <Outlet />
+      <div className="container mx-auto p-6 flex flex-col items-center">
+        <Outlet />
+      </div>
     </div>
   );
 };
